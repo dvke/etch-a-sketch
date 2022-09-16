ba1753624d989183c88e2fd74e3bda6190d4e02f
dvke <uchennaobiekwe@yahoo.com>
2022-09-16 14:01:25 +0100

    create 'draw' feature

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const container = document.getElementById("container");
 
+// function to create 16*16 grid
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
+
+    //change color of cell when mouse is over it
+    cell.addEventListener('mouseover', e=>setBgColor(cell));
+
+    function setBgColor(element){
+        element.style.backgroundColor = "blueviolet";
+    }
   };
+
+  
+
 };
 
 makeRows(16, 16);
+
